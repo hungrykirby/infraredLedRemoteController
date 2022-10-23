c8a551e29a1e3d2b2b7d0a884d90ad5bfe4bb2d2
hungrykirby <hungrykirby@users.noreply.github.com>
2022-10-23 19:06:55 +0900

    jsonにsignalsというパラメータを持たせた場合、その数字をいい感じに送信するようにした。 (#2)
    * receive signals
    
    * 配列の長さをいい感じに定義できるようにする
    
    * いらないコードを削除した

--- infraredLedRemoteController.ino
+++ infraredLedRemoteController.ino
@@ -38,16 +38,40 @@ String body = "Initial String";
 
 WebServer server(80);
 
-void toWhite() {
-  int data[] = {3575,1653,478,392,502,370,473,1262,503,1238,478,393,501,1238,505,364,501,372,500,371,500,1241,497,370,476,392,500,1239,502,367,499,1241,478,392,501,1237,501,370,476,395,499,1238,503,1236,478,1260,504,371,478,390,474,395,475,1266,498,371,499,1238,476,395,502,369,500,371,475,1263,478,1261,501,1238,503,370,499,370,475,1262,479,1260,477,395,500,1235,504};
-  int num_data = sizeof(data)/sizeof(data[0]);
-  turn(data, num_data);
-}
+int receiveSignals(String signals) {
+  int index = 0;
+  // arraySizeのロジックを追加すると-1が返ってしまうため削除した
+  // https://gangannikki.hatenadiary.jp/entry/2019/01/24/154015
+  // 動くはずなんだが....
+  // int arraySize = (sizeof(signals)/sizeof(signals[0]));  
+  unsigned long signalslength = signals.length();
 
-void toOrange() {
-  int data[] = {3547,1682,500,369,471,399,472,1265,473,1270,473,394,471,1271,471,399,444,426,447,422,447,1291,448,424,447,420,472,1269,449,420,472,1269,471,398,474,1264,449,424,472,397,473,1265,449,1289,448,1292,472,399,446,423,447,1294,448,1291,447,423,446,1293,448,422,448,421,473,398,470,1268,449,423,446,1291,448,424,446,424,449,1288,448,1292,470,401,470,1267,471};
-  int num_data = sizeof(data)/sizeof(data[0]);
-  turn(data, num_data);
+  // 配列を初期化するために長さをはかる
+  for (int i = 0; i < signalslength; i++) {
+    char tmp = signals.charAt(i);
+    if ( tmp == ',' ) {
+      index++;
+    }
+  }
+
+  String dst[index + 1] = {"\0"};
+  index = 0; // 再度oにする
+
+  for (int i = 0; i < signalslength; i++) {
+    char tmp = signals.charAt(i);
+    if ( tmp == ',' ) {
+      index++;
+      // if ( index > (arraySize - 1)) return -1;
+    }
+    else dst[index] += tmp;
+  }
+
+  int data[index + 1] = {-1};
+  for(int i = 0; i <= index; i++) {
+    data[i] = dst[i].toInt();
+  }
+  turn(data, index + 1);
+  return (index + 1);
 }
 
 void turn(int data[], int num_data) {
@@ -88,20 +112,16 @@ void setup() {
   Serial.println("Timer set to 5 seconds (timerDelay variable), it will take 5 seconds before publishing the first reading.");
 
   server.on("/control", HTTP_ANY, [](){
+    int result = 0;
     if (server.method() == HTTP_POST) { // POSTメソッドでアクセスされた場合
       body = server.arg("plain"); // server.arg("plain")でリクエストボディが取れる
       JSONVar json;
       json = JSON.parse(body);
-      if(json.hasOwnProperty("color")) {
-        if( String((const char*)json["color"]) == "white") {
-          toWhite();
-        } else if( String((const char*)json["color"]) == "orange") {
-          toOrange();
-        }
+      if(json.hasOwnProperty("signals")) {
+        result = receiveSignals(String((const char*)json["signals"]));
       }
-      Serial.println((const char*)json["color"]);
     }
-    server.send(200, "text/plain", body); // 値をクライアントに返す
+    server.send(200, "text/plain", (String) result + "\n"); // 値をクライアントに返す
   });
 
   // 登録されてないパスにアクセスがあった場合
